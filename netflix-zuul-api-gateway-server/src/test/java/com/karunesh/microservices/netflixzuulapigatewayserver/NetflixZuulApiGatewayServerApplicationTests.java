8466ddf897230d10c921eae9665bc6abe22cc673
Karunesh Maisalge <karuneshmaisalge@Karuneshs-MacBook-Air.local>
2020-06-18 08:31:07 +0530

    Micro services

---
+++ netflix-zuul-api-gateway-server/src/test/java/com/karunesh/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karunesh.microservices.netflixzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
